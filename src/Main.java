769dd60e9b50e6a4362ae7b496e51b1d240477eb
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-03-24 22:09:43 -0400

    Stage 2 of 5 for JetBrains Academy - Java - Tic-Tac-Toe project.
    • this stage has us receive a string that is to be formatted as a game of tic tac toe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X\nO X O\nX X O");
+        System.out.println("Enter cells: ");
+        String[] grid = new Scanner(System.in).next().split("(?<=\\G.{3})");
+        String dashes = "---------";
+
+        System.out.println(dashes);
+        for (String line : grid) {
+            char[] chars = line.toCharArray();
+            System.out.println("| " + chars[0] + " " + chars[1] + " " + chars[2] + " |");
+        }
+        System.out.println(dashes);
     }
 }
