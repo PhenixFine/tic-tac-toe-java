4548fe4ca4d0bbf49186321adb2a52fec871e107
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-04-03 00:44:13 -0400

    Stage 4 of 5 for JetBrains Academy - Java - Tic-Tac-Toe project.
    • program now has the user enter the games current state, and then asks the user for the next move. It then verifies if the next move is valid, if so it marks the spot with an X, if not, it re-asks the user for a valid move until one is entered. Methods printResults and checkWin from last stage were commented out, as Stage 4 mentioned that they would be needed for Stage 5.
    • main() - adjusted to use getString() and printGrid(). Removed the use of printResults()
    • getCell() - uses a try catch block to verify that the user entered numbers within range and not already occupied with X or O. If an exception is thrown the catch statement prints out the appropriate message and then recalls getCell() to try again.
    • getString() - prints the given text and then returns the next line as a string.
    • notRange() - returns true if given number is less than 1 or greater than 3

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Enter cells: ");
-        String combined = new Scanner(System.in).next();
+        String combined = getString("Enter cells: ");
         String[][] cells = {combined.substring(0, 3).split(""), combined.substring(3, 6).split(""),
                 combined.substring(6).split("")};
 
         printGrid(cells);
-        printResults(cells, combined);
+        getCell(cells, "X");
+        printGrid(cells);
     }
 
     private static void printGrid(String[][] cells) {
@@ -19,23 +19,49 @@ public class Main {
         System.out.println(dashes);
     }
 
-    private static void printResults(String[][] cells, String combined) {
-        int xCount = combined.length() - combined.replace("X", "").length();
-        int oCount = combined.length() - combined.replace("O", "").length();
-        boolean xWins = checkWin(cells, "X");
-        boolean oWins = checkWin(cells, "O");
-        boolean empty = combined.contains("_");
+    private static void getCell(String[][] cells, String letter) {
+        try {
+            String[] hold = getString("Enter the coordinates: ").split(" ");
+            int first = Integer.parseInt(hold[0]);
+            int second = Integer.parseInt(hold[1]);
 
-        System.out.println(xWins && oWins || Math.abs(xCount - oCount) > 1 ? "Impossible" : xWins ? "X wins" :
-                oWins ? "O wins" : empty ? "Game not finished" : "Draw");
-    }
-
-    private static Boolean checkWin(String[][] cells, String check) {
-        for (String[] line : cells) if ((line[0] + line[1] + line[2]).equals(check.repeat(3))) return true;
-        for (int i = 0; i < cells.length; i++) {
-            if (cells[0][i].equals(check) && cells[1][i].equals(check) && cells[2][i].equals(check)) return true;
+            if (notRange(first) || notRange(second)) throw new Exception("Coordinates should be from 1 to 3!");
+            else if (!cells[first - 1][second - 1].equals("_")) {
+                throw new Exception("This cell is occupied! Choose another one!");
+            } else cells[first - 1][second - 1] = letter;
+        } catch (Exception e) {
+            System.out.println(e instanceof NumberFormatException || e instanceof IndexOutOfBoundsException ?
+                    "You should enter numbers!" : e.getMessage());
+            getCell(cells, letter);
         }
-        if (cells[0][0].equals(check) && cells[1][1].equals(check) && cells[2][2].equals(check)) return true;
-        return cells[0][2].equals(check) && cells[1][1].equals(check) && cells[2][0].equals(check);
     }
+
+    private static String getString(String text) {
+        System.out.print(text);
+        return new Scanner(System.in).nextLine();
+    }
+
+    private static boolean notRange(int number) {
+        return number < 1 || number > 3;
+    }
+
+//    private static void printResults(String[][] cells, String combined) {
+//        int xCount = combined.length() - combined.replace("X", "").length();
+//        int oCount = combined.length() - combined.replace("O", "").length();
+//        boolean xWins = checkWin(cells, "X");
+//        boolean oWins = checkWin(cells, "O");
+//        boolean empty = combined.contains("_");
+//
+//        System.out.println(xWins && oWins || Math.abs(xCount - oCount) > 1 ? "Impossible" : xWins ? "X wins" :
+//                oWins ? "O wins" : empty ? "Game not finished" : "Draw");
+//    }
+//
+//    private static Boolean checkWin(String[][] cells, String check) {
+//        for (String[] line : cells) if ((line[0] + line[1] + line[2]).equals(check.repeat(3))) return true;
+//        for (int i = 0; i < cells.length; i++) {
+//            if (cells[0][i].equals(check) && cells[1][i].equals(check) && cells[2][i].equals(check)) return true;
+//        }
+//        if (cells[0][0].equals(check) && cells[1][1].equals(check) && cells[2][2].equals(check)) return true;
+//        return cells[0][2].equals(check) && cells[1][1].equals(check) && cells[2][0].equals(check);
+//    }
 }
